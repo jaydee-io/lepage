3f103e7b8ad63ba44780724a0d9058ee93cfc98b
Jérôme Dumesnil <jerome.dumesnil@gmail.com>
2021-03-28 17:58:43 +0200

    Make unit tests scripts independant

--- cmake/UnitTest.cmake
+++ cmake/UnitTest.cmake
@@ -1,23 +1,31 @@
 ################################################################################
-##                                    lepage
+##                             CMakeSupportScripts
 ##
 ## This file is distributed under the 4-clause Berkeley Software Distribution
 ## License. See LICENSE for details.
 ################################################################################
-if(NOT LEPAGE_BUILD_TESTS)
+if(NOT DEFINED BUILD_TESTS_OPTION_NAME)
+    string(TOUPPER "${CMAKE_PROJECT_NAME}_BUILD_TESTS" BUILD_TESTS_OPTION_NAME)
+endif()
+
+if(NOT ${BUILD_TESTS_OPTION_NAME})
     macro(enable_unit_test)
     endmacro()
     function(add_unit_test)
     endfunction()
 
+    message(STATUS "Enabling unit tests for '${CMAKE_PROJECT_NAME}': no")
+
     return()
 endif()
+
+message(STATUS "Enabling unit tests for '${CMAKE_PROJECT_NAME}': yes")
 
 macro(find_googletest)
     # First, try the system wide installation
     find_package(GTest)
     if(GTEST_FOUND)
-        set(LEPAGE_INTERNAL_UNIT_TESTS_LIBS GTest::GTest GTest::Main CACHE INTERNAL "INTERNAL Unit tests libraries to link")
+        set(UNIT_TESTS_LIBS GTest::GTest GTest::Main CACHE INTERNAL "INTERNAL Unit tests libraries to link")
     # If not available, try fetching from github
     else()
         message(STATUS "Fetching GoogleTest from GitHub")
@@ -37,7 +45,7 @@ macro(find_googletest)
         message(STATUS "Fetching GoogleTest from GitHub - done")
 
         include(GoogleTest)
-        set(LEPAGE_INTERNAL_UNIT_TESTS_LIBS gtest gmock gtest_main CACHE INTERNAL "INTERNAL Unit tests libraries to link")
+        set(UNIT_TESTS_LIBS gtest gmock gtest_main CACHE INTERNAL "INTERNAL Unit tests libraries to link")
     endif()
 endmacro()
 
@@ -47,8 +55,7 @@ macro(enable_unit_test)
 endmacro()
 
 function(add_unit_test TEST_TARGET)
-    add_executable(unittest-${TEST_TARGET} ${ARGN})
-    target_compile_features(unittest-${TEST_TARGET} PRIVATE ${LEPAGE_CXX_STANDARD})
-    target_link_libraries(unittest-${TEST_TARGET} ${LEPAGE_INTERNAL_UNIT_TESTS_LIBS})
-    gtest_discover_tests(unittest-${TEST_TARGET} EXTRA_ARGS --gtest_color=yes)
+    add_executable(${TEST_TARGET} ${ARGN})
+    target_link_libraries(${TEST_TARGET} ${UNIT_TESTS_LIBS})
+    gtest_discover_tests(${TEST_TARGET} EXTRA_ARGS --gtest_color=yes)
 endfunction()
