2493ee6f4dc6b666f1b498c1ecc9fa9ba66c59cd
Jérôme Dumesnil <jerome.dumesnil@gmail.com>
2021-05-14 12:05:15 +0200

    Fix TravisCI environment issue

--- cmake/UnitTest.cmake
+++ cmake/UnitTest.cmake
@@ -34,7 +34,6 @@ macro(find_googletest)
         FetchContent_Declare(
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG        release-1.10.0
         )
         
         FetchContent_GetProperties(googletest)
